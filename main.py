44e960bc5a7e9b97a51ab8f4991352df27e488a5
Infinite Souls <77794245+Infinitesouls@users.noreply.github.com>
2022-09-19 22:59:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,27 +16,18 @@ date = datetime.datetime.now()
 s = date.strftime('%c')
 
 os.system("cls||clear")
-fl = fgd("CYBERMAIL")
+fl = fgd("EMAIL SENDER")
 print(fl)
 print("\n")
-print(Fore.GREEN + "Anonymous Email Sender" + Style.RESET_ALL)
-print("By Using This Feature you will send anonymous email.")
-print("You're real email was encrypted. this method will recorded")
-print("You can see the current record by html file, the file will generated")
-print("after you send a email. and automaticaly deleted when you start new journey")
-print("\n")
-print(Fore.RED + "ATTENTION ALERT FOR USER" + Style.RESET_ALL)
-print("This method is illegal, don't use this method for scamming")
-print("criminal issues, or some criminal case")
+print(Fore.GREEN + "Email Sender" + Style.RESET_ALL)
 print("\n")
 
-sender_email = "antromaxxcii@gmail.com"
-rec_email = input(Fore.RED + "[+] " + Style.RESET_ALL + "Rechiever Email [Username] > ")
-user_name = input(Fore.RED + "[+] " + Style.RESET_ALL + "Your Name [Username] > ")
-receiver_email = rec_email + "@gmail.com"
+sender_email = "Your Mail"
+receiver_email = input(Fore.RED + "[+] " + Style.RESET_ALL + "Rechiever Email > ")
+receiver_email
 subject = input(Fore.RED + "[+] " + Style.RESET_ALL + "Subject > ")
 pesan = input(Fore.RED + "[+] " + Style.RESET_ALL + "Message > ")
-password = "wdtxourvbunortmz"
+password = "Key App Password"
     
 print("\n")
 print("Information Services [ EMAIL STATUS ]")
@@ -50,43 +41,14 @@ print(myTable)
 
 message = MIMEMultipart("alternative")
 message["Subject"] = subject
-message["From"] = "The Second Ronin"
+message["From"] = "Random Mail"
 message["To"] = receiver_email
 
     # Create the plain-text and HTML version of your message
 html = f"""\
     <html>
     <body>
-        <div class="border-content" style="border : 1px solid #ddd; height : 100vh; width: 100%; border-radius: 5px; height : max-content;background : white;">
-            <div class="content-box" style="background: linear-gradient(160deg, rgba(19,1,107,1) 5%, rgba(255,78,218,1) 100%); margin : 0; padding : 0;">
-                <div class="header" style="font-family: arial; background: none; color : white; padding : 20px; font-weight: bold;">
-                    Greeper.io <br><span style="font-weight :lighter; opacity: .8; font-size: small;">Third Party Email Service Manager</span>
-                </div>
-            </div>
-
-            <div class="sender" style="margin: 0; padding : 0; margin-top: -10px; background : white!important;">
-                <div class="cardHe" style="display : flex ; padding : 20px; align-items : center; font-family: Arial; margin-top : 5px;">
-                    <img style="border : 1px solid #ddd; padding : 1px; border-radius : 100%;" src="https://cdn.discordapp.com/attachments/1014711843614367757/1020980413704970260/20220917_204611.png" alt="" width="40">
-
-                    <div class="sa" style="margin-left : 10px;">
-                        <span class="jd" style="font-weight: bold; font-size : large;">{user_name}</span><br>
-                    </div>
-                </div>
-
-                <div class="msga" style="font-family: arial; margin : 15px; margin-top : -20px; border-radius : 2px; padding : 10px; background : white!important;">
-                    {pesan}
-                </div>
-
-                <div class="description-text">
-                    <div class="text" style=" margin : 0px; margin-top: -10px; color : black; font-family: Arial, Helvetica, sans-serif; padding : 20px; font-size: medium; width: max-content; overflow: hidden;">
-                        Device Name As <b>{platform.node()}</b>
-
-                    </div>
-                  
-                </div>
-
-            </div>
-        </div>
+        <p>{pesan}</p>
     </body>
     </html>
     """
