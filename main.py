7f7a77dd81d5fcce6bc17ff7b4a210f0be812ae7
Mexaminz <77794245+Infinitesouls@users.noreply.github.com>
2022-11-05 17:18:08 +0700

    Update Mailer
    Removing useless command and fixing some language error.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from email.message import EmailMessage
 from platform import platform
 import time, datetime, colorama
-from traceback import print_tb
 from colorama import Fore, Back , Style
 import os
 import pyfiglet
@@ -26,13 +25,13 @@ sender_email = "Your Mail"
 receiver_email = input(Fore.RED + "[+] " + Style.RESET_ALL + "Rechiever Email > ")
 receiver_email
 subject = input(Fore.RED + "[+] " + Style.RESET_ALL + "Subject > ")
-pesan = input(Fore.RED + "[+] " + Style.RESET_ALL + "Message > ")
+message_div = input(Fore.RED + "[+] " + Style.RESET_ALL + "Message > ")
 password = "Key App Password"
     
 print("\n")
 print("Information Services [ EMAIL STATUS ]")
 myTable = PrettyTable(["Author", "Email Sender", "Rechiever", "Message Length", "Timestamp"])
-myTable.add_row([platform.node(), sender_email, receiver_email, len(pesan), s])
+myTable.add_row([platform.node(), sender_email, receiver_email, len(message_div), s])
 print(myTable)
   
 
@@ -45,16 +44,16 @@ message["From"] = "Random Mail"
 message["To"] = receiver_email
 
     # Create the plain-text and HTML version of your message
-html = f"""\
+email_html = f"""\
     <html>
     <body>
-        <p>{pesan}</p>
+        <p>{message_div}</p>
     </body>
     </html>
     """
 
     # Turn these into plain/html MIMEText objects
-mailTest = MIMEText(html, "html")
+mailTest = MIMEText(email_html, "html")
 
     # Add HTML/plain-text parts to MIMEMultipart message
     # The email client will try to render the last part first
